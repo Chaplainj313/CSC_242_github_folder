ce39c0a6512201a1a0d6d7a9b625b28af38cc8ae
Joshua Chaplain <41711214+Chaplainj313@users.noreply.github.com>
2021-04-25 21:46:03 -0700

    changes to area program

---
+++ Source.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//This program prompts user to enter circle raduis
+//Then the circle area is displayed
+
+int main()
+{
+
+	//Declare variables
+	double radius, area;
+	const double PI = 3.14159;
+
+
+	//prompt user for radius
+	cout << "Enter radius\n";
+	cin >> radius;
+
+	//calculate the area
+	area = PI * pow(radius, 2);
+
+
+	cout << "The area of a circle with radius " << radius << " is " << area << endl;
+
+
+
+
+	cout << sqrt(4);
+
+
+	//Calculate the area of the circle
+	return 0;
+}
+
+
+int alt ( ){
+
+}
+
